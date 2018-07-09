5cbf0da6584259e0276624c51b1e6fd711ce05b8
arriverwei <492747040@qq.com>
2018-07-09 17:26:34 +0800

    textnew

--- jumei/js/details.js
+++ jumei/js/details.js
@@ -3,6 +3,9 @@ function glass(){
 	this.movebox=document.getElementsByClassName('movebox')[0];
 	this.showimg=document.getElementsByClassName('showimg')[0];
 	this.imgbox=document.getElementsByClassName('imgbox')[0];
+	this.main=document.getElementsByClassName('main')[0];
+	this.clearfix=document.getElementsByClassName('clearfix')[0];
+	this.body=document.getElementsByTagName('body')[0]
 	var _this=this;
 	this.showimg.onmouseover=function(){
 		_this.left()
@@ -24,11 +27,18 @@ function glass(){
 	}
 	glass.prototype.move=function(ev){
 		var	ev=ev||window.event;
-		
-		this.width=ev.clientX-this.showimg.offsetLeft-this.movebox.offsetWidth/2;
-		this.height=ev.clientY-this.showimg.offsetTop-this.movebox.offsetHeight/2;
+		this.width=ev.clientX-this.main.offsetLeft-this.movebox.offsetWidth/2;
 		this.maxWidth=this.showimg.clientWidth-this.movebox.offsetWidth
+		if(document.documentElement.scrollTop){
+		this.height=ev.clientY-this.clearfix.offsetTop-this.movebox.offsetHeight/2+document.documentElement.scrollTop;
 		this.maxHeight=this.showimg.clientHeight-this.movebox.offsetHeight
+		
+		}else{
+			this.height=ev.clientY-this.clearfix.offsetTop-this.movebox.offsetHeight/2;
+		this.maxHeight=this.showimg.clientHeight-this.movebox.offsetHeight
+		}
+		
+		
 		if(this.width>this.maxWidth){
 			this.width=this.maxWidth
 		}
@@ -46,4 +56,4 @@ function glass(){
 		this.imgbox.querySelector('img').style.left=this.width*(-2)+'px'
 		this.imgbox.querySelector('img').style.top=this.height*(-2)+'px'
 	}
-
+    
